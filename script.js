892da57ab2b749e173865092cbc6bd838ee3fdf5
Lucas Florentino <lucas7florentino@hotmail.com>
2025-04-01 13:29:47 -0300

    css and html complete

---
+++ script.js
@@ -0,0 +1,4 @@
+const checkBtn = document.getElementById("check-btn");
+const userInput = document.getElementById("user-input");
+const clearButton = document.getElementById("clear-btn");
+const resultsDiv = document.getElementById("results-div")
